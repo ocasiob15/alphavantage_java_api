f0f4353bddfcb0e17e32d72de073039ec029bf66
Brian Ocasio <brianocasio@soonersofts-MacBook-Pro.local>
2018-04-29 16:33:01 -0400

    Added MACDObject

--- alphavantage/MACDObject.java
+++ alphavantage/MACDObject.java
@@ -1,1 +1,14 @@
 
+package alphavantage;
+
+public class MACDObject {
+    public String stock_ticker;
+    public String stock_time;
+    public double MACD;
+
+    public MACDObject(String stock_ticker, String stock_time, double MACD){
+        this.stock_ticker = stock_ticker;
+        this.stock_time = stock_time;
+        this.MACD = MACD;
+    }
+}
