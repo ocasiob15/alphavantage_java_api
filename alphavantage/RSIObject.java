cf0dab4592e2620034f2237a64dabbb84b22e21c
Brian Ocasio <brianocasio@soonersofts-MacBook-Pro.local>
2018-04-29 16:27:14 -0400

    Added RSIObject

--- alphavantage/RSIObject.java
+++ alphavantage/RSIObject.java
@@ -1,1 +1,14 @@
 
+package alphavantage;
+
+public class RSIObject {
+    public String stock_ticker;
+    public String stock_time;
+    public double rsi;
+
+    public RSIObject(String stock_ticker, String stock_time, double rsi){
+        this.stock_ticker = stock_ticker;
+        this.stock_time = stock_time;
+        this.rsi = rsi;
+    }
+}
